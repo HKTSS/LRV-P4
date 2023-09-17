00736d27a6ef7328bac39cf935419cce6ef44ee0
LX862 <kenny.mh.hui@outlook.com>
2023-09-17 19:05:59 +0800

    Sync to single car variant

---
+++ HKTSS MTR Light Rail Phase IV/3Dcab/Wiper/wiper.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using OpenBveApi.Hosts;
+using OpenBveApi.Interface;
+using OpenBveApi.Math;
+using OpenBveApi.Trains;
+
+public class OpenBVEScript {
+
+	private double ElapsedTime;
+	private int CurrentWiperMode;
+	private bool isZAxis;
+	private bool isVertical;
+
+	public OpenBVEScript(Dictionary<string, string> args) {
+		isVertical = args["vertical"] == "true";
+		isZAxis = args["isZ"] == "true";
+	}
+
+	public double ExecuteScript(AbstractTrain Train, int CarIndex, Vector3 Position, double TrackPosition, int SectionIndex, bool IsPartOfTrain, double TimeElapsed, int CurrentState) {
+		if(CarIndex <= 0) {
+			ElapsedTime += TimeElapsed;
+		}
+		
+		double wiperProgress = GetWiperProgress();
+		
+		if(CurrentWiperMode != CurrentState && wiperProgress <= 0.02) {
+			CurrentWiperMode = CurrentState;
+			ElapsedTime = TimeElapsed;
+		}
+		
+		return CarIndex > 0 ? GetWiperValue(0) : GetWiperValue(wiperProgress);
+	}
+	
+	private double GetWiperProgress() {
+		switch(CurrentWiperMode) {
+			case 1:
+				return Math.Pow(Math.Sin(ElapsedTime / 2), 100);
+			case 2:
+				return Math.Pow(Math.Sin(ElapsedTime / 1.5), 50);
+			case 3:
+				return Math.Pow(Math.Sin(ElapsedTime), 26);
+			case 4:
+				return Math.Pow(Math.Sin(ElapsedTime/0.5), 4);
+			default:
+				return 0;
+		}
+	}
+	
+	private double GetWiperValue(double wiperProgress) {
+		if(isVertical) {
+			if(isZAxis) {
+				return wiperProgress;
+			} else {
+				return 0.05 * wiperProgress;
+			}
+		} else {
+			if(isZAxis) {
+				return 1.5 * wiperProgress;
+			} else {
+				return 0.5 - (wiperProgress / 2);
+			}
+		}
+	}
+}
---
+++ HKTSS MTR Light Rail Phase IV/vram_calc.py
@@ -0,0 +1,29 @@
+import glob
+from PIL import Image
+import math
+
+# root_dir needs a trailing slash (i.e. /root/dir/)
+
+dimlist = {}
+
+def uppotwo(x):
+    return pow(2, math.ceil(math.log(x)/math.log(2)))
+
+for filename in glob.iglob('**/*.png', recursive=True):
+    if "train.png" in filename or "Temp_ReplicaSticker" in filename or "/Dark/" in filename or "src/" in filename:
+        continue
+    with Image.open(filename) as image:
+         w, h = image.size
+         # https://github.com/leezer3/OpenBVE/issues/226
+         wW = uppotwo(w)
+         hH = uppotwo(h)
+         vram = (wW * 4) * (hH)
+         dimlist[filename] = vram
+
+total = 0
+for path in sorted(dimlist, key=dimlist.get):
+    vram = dimlist[path]
+    total += vram
+    print(str(vram / 1048576) + "MB" + ": " + path)
+
+print("Total: " + str(total / 1048576) + "MB")
